fda321b1a9915f56860ed43dfe186a9db09d601e
mughees605 <mughees605@gmail.com>
2017-07-21 16:58:27 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import {router} from "./src/routes/routes";
+import express from "express"
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/api',router)
+
+
+// mongoose.connect('mongodb://mughees605:mughees1997@ds163701.mlab.com:63701/es6-mongoose')
+app.listen(port,() => {
+    console.log(port)
+})
